ec6a1c509fa83505b51dc678f06dd583ab2875bb
Doan Sy Hung <hungdoansy@gmail.com>
2022-03-20 20:07:52 +0700

    chore: update contact info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,30 @@ const GithubSVG = () => {
     )
 }
 
+const EmailSVG = () => {
+    return (
+        <span className="logo">
+            <svg width="1em" height="1em" viewBox="0 0 24 24">
+                <path
+                    fill="currentColor"
+                    d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm-.4 4.25l-7.07 4.42c-.32.2-.74.2-1.06 0L4.4 8.25a.85.85 0 1 1 .9-1.44L12 11l6.7-4.19a.85.85 0 1 1 .9 1.44z"
+                />
+            </svg>
+            <style jsx>{`
+                .logo {
+                    display: inline-block;
+                    height: 1em;
+                    vertical-align: -2px;
+
+                    > svg {
+                        height: 100%;
+                    }
+                }
+            `}</style>
+        </span>
+    )
+}
+
 export const getStaticProps: GetStaticProps<Props> = async (context) => {
     const collections = getRecentCollections()
     const notes = collections
@@ -62,10 +86,10 @@ const HomePage: NextPage<Props> = ({ notes }) => {
                 <p>In case you&apos;re interested:</p>
                 <ul style={{ listStyle: "none" }}>
                     <li>
-                        -&gt; <Link href="https://github.com/hungdoansy">Find me on GitHub</Link>
+                        -&gt; <Link href="https://github.com/hungdoansy">Find me on GitHub</Link> <GithubSVG />
                     </li>
                     <li>
-                        -&gt; <Link href="mailto:hey@syhung.me">Contact me</Link>
+                        -&gt; <Link href="mailto:hungdoansy@gmail.com">Contact me</Link> <EmailSVG />
                     </li>
                 </ul>
 
