ed53cd7beb41da398aafab62d055787a7f80c05a
Hung Doan <hungdoansy@gmail.com>
2024-04-22 21:13:38 +0700

    feat: xam lin page

---
+++ src/pages/xam-lin/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next"
+
+const Page: NextPage = () => {
+    console.log("MjE2MjEzMzkwMDk3Nzk0")
+
+    return (
+        <main className="github-theme">
+            <h1>Ở đây không có gì cả</h1>
+        </main>
+    )
+}
+
+export default Page
